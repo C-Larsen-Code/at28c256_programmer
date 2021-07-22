2d263c9bb53c1b443bdab67a0f95c984d838f4f4
Dr-Tronic <cjlar2002@gmail.com>
2021-07-22 16:45:42 -0600

    The default constructor now works. The 'startRead' and 'printInternalVals' keywords do not appear in orange like they should.

--- EEPROM_Comm.cpp
+++ EEPROM_Comm.cpp
@@ -34,18 +34,12 @@ EEPROM::EEPROM(char fileName[], const int clockTime,int chipSelectPin,
 }
 
 EEPROM::EEPROM(char fileName[]) {
-	//Pin A7 is the highest order Data bit (D7)
-	const byte DATA[] = {A7, A6, A5, A4, A3, A2, A1, A0};
-	//Pin 47 is the (second) highest order Address bit (A14)
-	const byte ADDR[] = {33, 34, 35, 36, 37, 38, 39, 40,
-						 41, 42, 43, 44, 45, 46, 47};
-	
 	_fileName = fileName;
-	_clockTime = 100;
+	_clockTime = 10000;
 	
 	_chipSelectPin = 53; 
-	_DATA = DATA;
-	_ADDR = ADDR;
+	_DATA = default_DATA;
+	_ADDR = default_ADDR;
 	_writePin = A9;
 	_OEPin = A8;
 }
@@ -58,6 +52,89 @@ void EEPROM::setAddress(unsigned int twoByte) {
 	return;
 }
 /************************************************************************/
+void EEPROM::autoSetup(void){
+	Serial.begin(SERIAL_SPEED);
+	  
+	if (!SD.begin(_chipSelectPin)) {
+		Serial.println("SD card initialization failed.");
+		_SDCardReady = false;
+		_myFile = SD.open(_fileName);
+		//while (1);
+	} else {
+		  Serial.println("SD card initialization complete.");
+
+		  if (!(_myFile = SD.open(_fileName))) {
+			Serial.println("Error opening file");
+			_SDCardReady = false;
+			//while(1);
+		  } else {
+			_SDCardReady = true;
+		  }
+	}
+	  pinMode(_writePin, OUTPUT);
+	  pinMode(_OEPin, OUTPUT);
+
+	  digitalWrite(_writePin, HIGH);
+	  digitalWrite(_OEPin, HIGH);
+
+	  for(int i=0; i<15; i++){
+		pinMode(_ADDR[i], OUTPUT);
+		digitalWrite(_ADDR[i], LOW);
+	  }
+	  return;
+}
+/************************************************************************/
+void EEPROM::printInternalVals(void) {
+	char buffer[101];
+	char smallBuffer[6];
+
+	Serial.println();
+
+	// File Name
+	sprintf(buffer, "_fileName\t:\t%s", _fileName);
+	Serial.println(buffer);
+
+	// Clock Time
+	sprintf(buffer, "_clockTime\t:\t%d", _clockTime);
+	Serial.println(buffer);
+
+	// Chip Select Pin
+	sprintf(buffer, "_chipSelectPin\t:\t%d", _chipSelectPin);
+	Serial.println(buffer);
+
+	// Write Enable Pin
+	sprintf(buffer, "_writePin\t:\t%d", _writePin);
+	Serial.println(buffer);
+
+	// Output Enable Pin
+	sprintf(buffer, "_OEPin\t\t:\t%d", _OEPin);
+	Serial.println(buffer);
+
+	// Data Lines
+	sprintf(buffer, "Data\t\t:\t");
+	for (int i=0; i<7; i++) {
+		sprintf(smallBuffer, "%d, ", _DATA[i]);
+		strcat(buffer, smallBuffer);
+	}
+	sprintf(smallBuffer, "%d", _DATA[7]);
+	strcat(buffer, smallBuffer);
+	Serial.println(buffer);
+
+	// Address Lines
+	sprintf(buffer, "Address\t\t:\t");
+	for (int i=0; i<14; i++) {
+		sprintf(smallBuffer, "%d, ", _ADDR[i]);
+		strcat(buffer, smallBuffer);
+	}
+	sprintf(smallBuffer, "%d", _ADDR[14]);
+	strcat(buffer, smallBuffer);
+	Serial.println(buffer);
+
+	Serial.println();
+
+	return;
+}
+/************************************************************************/
 void EEPROM::startWrite(){
 	for(int i=0; i<8; i++){
 		pinMode(_DATA[i], OUTPUT);
@@ -65,63 +142,6 @@ void EEPROM::startWrite(){
 	}
 	digitalWrite(_OEPin, HIGH);
 	Serial.println("Beginning write:");
-	return;
-}
-/************************************************************************/
-void EEPROM::startRead(){
-	for(int i=0; i<8; i++){
-		pinMode(_DATA[i], INPUT);
-	}
-	digitalWrite(_OEPin, LOW);
-	Serial.println("Ready to read values");
-	// The delay is to ensure correct reading of the first three-ish addresses
-	delay(100);
-	return;
-}
-/************************************************************************/
-void EEPROM::hexdump(int numOfLines){
-	// 55 characters per line (plus ending null character)
-	char *outString = (char*) calloc(numOfLines * HEXDUMP_LINE, sizeof(char));
-	char buffer[6] = {0};
-	for (int i = 0; i < numOfLines * 0x10; i+=0x10) {
-		
-		sprintf(buffer, "%0.4x ", i);
-		strcat(outString, buffer);
-		
-		for (int j = 0; j < 0x10; j++) {
-			unsigned int dataValue = 0;
-			// Read in all 8 data bits
-			for(int k = 0; k < 8; k++){
-				setAddress(i+j);
-				delayMicroseconds(50);
-				dataValue = (dataValue << 1) + (digitalRead(_DATA[k]) ? 1 : 0);
-			}
-			sprintf(buffer, " %0.2x", dataValue);
-			if (j == 0x7) strcat(buffer, " ");
-			else if (j == 0xF)
-				if (i == numOfLines - 1) strcat (buffer, "\0");
-				else strcat(buffer, "\n"); 
-			
-			strcat(outString, buffer);
-		}
-	}
-	Serial.print(outString);
-	return;
-}
-/************************************************************************/
-void EEPROM::readData(long int startAddress, long int howManyAddresses){
-	for(long int i = startAddress; i < startAddress + howManyAddresses; i++){
-		unsigned long int dataValue = 0;
-		char printString[40];
-		
-		for(int j = 0; j < 8; j++){
-			setAddress(i);
-			delayMicroseconds(50);
-			dataValue = (dataValue << 1) + (digitalRead(_DATA[j]) ? 1 : 0);
-		}
-		sprintf(printString, "Address:  0x%.4lx   Data: 0x%.2x\n", i, dataValue);
-		Serial.print(printString);
-	}
 	return;
 }
 /************************************************************************/
@@ -161,35 +181,60 @@ void EEPROM::writeDataSD(int programLength){
 	return;
 }
 /************************************************************************/
-void EEPROM::autoSetup(void){
-	Serial.begin(SERIAL_SPEED);
-	  
-	if (!SD.begin(_chipSelectPin)) {
-		Serial.println("SD card initialization failed.");
-		_SDCardReady = false;
-		_myFile = SD.open(_fileName);
-		//while (1);
-	} else {
-		  Serial.println("SD card initialization complete.");
-
-		  if (!(_myFile = SD.open(_fileName))) {
-			Serial.println("Error opening file");
-			_SDCardReady = false;
-			//while(1);
-		  } else {
-			_SDCardReady = true;
-		  }
+void EEPROM::startRead(){
+	for(int i=0; i<8; i++){
+		pinMode(_DATA[i], INPUT);
 	}
-	  pinMode(_writePin, OUTPUT);
-	  pinMode(_OEPin, OUTPUT);
-
-	  digitalWrite(_writePin, HIGH);
-	  digitalWrite(_OEPin, HIGH);
-
-	  for(int i=0; i<15; i++){
-		pinMode(_ADDR[i], OUTPUT);
-		digitalWrite(_ADDR[i], LOW);
-	  }
-	  return;
+	digitalWrite(_OEPin, LOW);
+	Serial.println("Ready to read values");
+	// The delay is to ensure correct reading of the first three-ish addresses
+	delay(100);
+	return;
+}
+/************************************************************************/
+void EEPROM::readData(long int startAddress, long int howManyAddresses){
+	for(long int i = startAddress; i < startAddress + howManyAddresses; i++){
+		unsigned long int dataValue = 0;
+		char printString[40];
+		
+		for(int j = 0; j < 8; j++){
+			setAddress(i);
+			delayMicroseconds(50);
+			dataValue = (dataValue << 1) + (digitalRead(_DATA[j]) ? 1 : 0);
+		}
+		sprintf(printString, "Address:  0x%.4lx   Data: 0x%.2x\n", i, dataValue);
+		Serial.print(printString);
+	}
+	return;
+}
+/************************************************************************/
+void EEPROM::hexdump(int numOfLines){
+	// 55 characters per line (plus ending null character)
+	char *outString = (char*) calloc(numOfLines * HEXDUMP_LINE, sizeof(char));
+	char buffer[6] = {0};
+	for (int i = 0; i < numOfLines * 0x10; i+=0x10) {
+		
+		sprintf(buffer, "%0.4x ", i);
+		strcat(outString, buffer);
+		
+		for (int j = 0; j < 0x10; j++) {
+			unsigned int dataValue = 0;
+			// Read in all 8 data bits
+			for(int k = 0; k < 8; k++){
+				setAddress(i+j);
+				delayMicroseconds(50);
+				dataValue = (dataValue << 1) + (digitalRead(_DATA[k]) ? 1 : 0);
+			}
+			sprintf(buffer, " %0.2x", dataValue);
+			if (j == 0x7) strcat(buffer, " ");
+			else if (j == 0xF)
+				if (i == numOfLines - 1) strcat (buffer, "\0");
+				else strcat(buffer, "\n"); 
+			
+			strcat(outString, buffer);
+		}
+	}
+	Serial.print(outString);
+	return;
 }
 /************************************************************************/
--- EEPROM_Comm.h
+++ EEPROM_Comm.h
@@ -25,18 +25,33 @@ class EEPROM {
 *	Function Declarations
 **************************************************************************/
 	public:
+		// Constructors
 		EEPROM(char fileName[], const int clockTime,int chipSelectPin,
 			   const byte *DATA, const byte *ADDR, const byte writePin,
 			   const byte OEPin);
 		EEPROM(char fileName[]);
+
+		// Setting the address is used in reading and writing. Could have
+		// made it private, but it comes in handy occasionally while debugging
 		void setAddress(unsigned int twoByte);
+
+		// Sets up Serial Monitor, opens up SD card file, and sets all of the
+		// pins (except for the data bus which is handled in startRead and
+		// startWrite)
+		void autoSetup(void);
+
+		// Used for debugging the internal values of the object
+		void printInternalVals(void);
+
+		// Writing functions
 		void startWrite();
+		void writeData(byte inputData, unsigned int address);
+		void writeDataSD(int programLength);
+
+		// Reading functions
 		void startRead();
 		void readData(long int startAddress, long int howManyAddresses);
 		void hexdump(int numOfLines);
-		void writeData(byte inputData, unsigned int address);
-		void writeDataSD(int programLength);
-		void autoSetup(void);
 /************************************************************************/
 	private:
 /*************************************************************************
@@ -55,6 +70,11 @@ class EEPROM {
 		int _chipSelectPin;
 		const byte *_DATA;
 		const byte *_ADDR;
+		// Pin A7 is the highest order Data bit (D7)
+		const byte default_DATA[8] = {A7, A6, A5, A4, A3, A2, A1, A0};
+		// Pin 47 is the (second) highest order Address bit (A14)
+		const byte default_ADDR[15] = {33, 34, 35, 36, 37, 38, 39, 40,
+									 41, 42, 43, 44, 45, 46, 47};
 		byte _writePin;
 		byte _OEPin;
 /************************************************************************/
